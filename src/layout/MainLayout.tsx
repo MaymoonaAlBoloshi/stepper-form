6761570b60bcd774a29f5f0b6df7a6a59d69089a
reemalqarni <reem.qarni@rihal.om>
2024-10-15 09:56:40 +0400

    app layout changes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,20 @@
+import { ReactNode } from 'react'; // Use ReactNode for typing children
 
-function MainLayout() {
-
-  return (
-    <>
-     <h1>Hello World!</h1>
-    </>
-  )
-}
-
-export default MainLayout;
+type Props = {
+    children: ReactNode; // This covers any valid JSX content
+  }
+  
+  function MainLayout({ children }: Props) {
+    return (
+      <>
+        <main>
+          <div>
+            <h1>Hello World!</h1>
+          </div>
+        </main>
+      </>
+    );
+  }
+  
+  export default MainLayout;
+  
